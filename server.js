567187456dcf117bd309cfd24faa21b22b22385e
Clarke Fry <clarkefry@gmail.com>
2018-02-10 09:02:07 -0600

    updating mongo connection

--- server.js
+++ server.js
@@ -30,9 +30,8 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kc-star-scrape
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-//   useMongoClient: true
-});
+mongoose.connect(MONGODB_URI)
+
 
 
 /////////////////////////////////////////////////////////
